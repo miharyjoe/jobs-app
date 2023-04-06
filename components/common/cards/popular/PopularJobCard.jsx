68a9aeb46a3d1e4ae746143c39ef372b03730601
Mihary Joel <miharyjoel@gmail.com>
2023-04-07 01:02:36 +0300

    feat: add card jobs

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -8,7 +8,24 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
       onPress={() => handleCardPress(item)}
-    ></TouchableOpacity>
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{ uri: item.employer_logo }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
