928e1754c3efa7a81b336efe2e2ab338d9ea4f9f
miharyjoel <miharyjoel@gmail.com>
2024-02-22 21:36:32 +0300

    add config build

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -10,7 +10,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "",
+      "X-RapidAPI-Key": process.env.EXPO_PUBLIC_RAPID_API_KEY,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
